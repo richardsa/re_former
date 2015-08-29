7efc6b784a949b7412689c598da21bb6b36fa6ed
richie <rsaldiva@gmail.com>
2015-08-28 22:15:48 -0400

    finish edit section

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,16 @@
     @user = User.find(params[:id])
   end
   
+def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+       redirect_to edit_user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+  
     private
   
     def user_params
